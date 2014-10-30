da8fb6571190c7b3da9579fec551f102eed532b7
Blair McArthur <mmkayjimmy@gmail.com>
2014-10-31 08:46:42 +1300

    moved the plugin.xml back into the root directory

--- CarnivalCordovaDemo/platforms/ios/www/cordova_plugins.js
+++ CarnivalCordovaDemo/platforms/ios/www/cordova_plugins.js
@@ -1,17 +1,7 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-    {
-        "file": "plugins/com.carnival.carnivalcordovaplugin/iOS/carnivalcordovaplugin.js",
-        "id": "com.carnival.carnivalcordovaplugin.CarnivalCordovaPlugin",
-        "clobbers": [
-            "CarnivalCordovaPlugin"
-        ]
-    }
-];
+module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
-{
-    "com.carnival.carnivalcordovaplugin": "1.0"
-}
+{}
 // BOTTOM OF METADATA
 });
