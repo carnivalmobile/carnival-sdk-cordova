73d88f50e8e4eebcb001dfa3aad815c6b26b445a
Blair McArthur <mmkayjimmy@gmail.com>
2014-11-03 10:34:24 +1300

    updated the demo app with the latest plugin

---
+++ CarnivalCordovaDemo/plugins/com.carnivalmobile.carnivalcordovaplugin/CarnivalCordovaDemo/platforms/ios/www/plugins/com.carnivalmobile.carnivalcordovaplugin/iOS/carnivalcordovaplugin.js
@@ -0,0 +1,27 @@
+cordova.define("com.carnivalmobile.carnivalcordovaplugin.CarnivalCordovaPlugin", function(require, exports, module) { function CarnivalCordovaPlugin() {}
+
+// Initialization
+CarnivalCordovaPlugin.prototype.startEngine = function() {
+    cordova.exec(null, null, "CarnivalCordovaPlugin", "startEngine", []);
+};
+
+// Tags
+CarnivalCordovaPlugin.prototype.getTags = function(onSuccess, onError) {
+    cordova.exec(onSuccess, onError, "CarnivalCordovaPlugin", "getTags", []);
+};
+
+CarnivalCordovaPlugin.prototype.setTags = function(onSuccess, onError, newTags) {
+    cordova.exec(onSuccess, onError, "CarnivalCordovaPlugin", "setTags", newTags);
+};
+
+CarnivalCordovaPlugin.prototype.addTags = function(onSuccess, onError, tagsToAdd) {
+    cordova.exec(onSuccess, onError, "CarnivalCordovaPlugin", "addTags", tagsToAdd);
+};
+
+// Stream
+CarnivalCordovaPlugin.prototype.showMessageStream = function() {
+    cordova.exec(null, null, "CarnivalCordovaPlugin", "showMessageStream", []);
+};
+
+module.exports = new CarnivalCordovaPlugin();
+});
