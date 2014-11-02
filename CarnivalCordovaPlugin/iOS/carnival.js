79392e4f5673bf80dcc5786617783c57aa595205
Blair McArthur <mmkayjimmy@gmail.com>
2014-11-03 10:53:18 +1300

    renamed the CarnivalCordovaPlugin Carnival and renamed the js file to Carnival.js

--- CarnivalCordovaPlugin/iOS/carnival.js
+++ CarnivalCordovaPlugin/iOS/carnival.js
@@ -1,26 +1,26 @@
-function CarnivalCordovaPlugin() {}
+function Carnival() {}
 
 // Initialization
-CarnivalCordovaPlugin.prototype.startEngine = function() {
+Carnival.prototype.startEngine = function() {
     cordova.exec(null, null, "CarnivalCordovaPlugin", "startEngine", []);
 };
 
 // Tags
-CarnivalCordovaPlugin.prototype.getTags = function(onSuccess, onError) {
+Carnival.prototype.getTags = function(onSuccess, onError) {
     cordova.exec(onSuccess, onError, "CarnivalCordovaPlugin", "getTags", []);
 };
 
-CarnivalCordovaPlugin.prototype.setTags = function(onSuccess, onError, newTags) {
+Carnival.prototype.setTags = function(onSuccess, onError, newTags) {
     cordova.exec(onSuccess, onError, "CarnivalCordovaPlugin", "setTags", newTags);
 };
 
-CarnivalCordovaPlugin.prototype.addTags = function(onSuccess, onError, tagsToAdd) {
+Carnival.prototype.addTags = function(onSuccess, onError, tagsToAdd) {
     cordova.exec(onSuccess, onError, "CarnivalCordovaPlugin", "addTags", tagsToAdd);
 };
 
 // Stream
-CarnivalCordovaPlugin.prototype.showMessageStream = function() {
+Carnival.prototype.showMessageStream = function() {
     cordova.exec(null, null, "CarnivalCordovaPlugin", "showMessageStream", []);
 };
 
-module.exports = new CarnivalCordovaPlugin();
+module.exports = new Carnival();
